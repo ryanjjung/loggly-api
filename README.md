bbac741a4983374e8064b33d374c1fb0ad3c206e
Ryan Jung <ryanjjung@gmail.com>
2018-04-02 18:33:50 -0600

    Initial working commit with a good readme

--- README.md
+++ README.md
@@ -2,46 +2,151 @@
 
 ## Description
 
-This library allows a user to work with the Loggly API - including account management, log shipping,
-and event retrieval - through simple function calls.
+This library allows a user to work with Loggly's APIs through simple function calls.
+
+## Installation
+
+    pip install loggly-api
+
+You're done.
 
 ## Usage
 
-    # Imports
-    import json
     import loggly
-    
-    # Configure
-    loggly.customer_token = 'your_customer'
+
+You can set various access credentials; different ones are needed for each operation.
+
+    loggly.token = 'your_customer_token'
+    loggly.api_token = 'your_api_token'
     loggly.user = 'your_username'
     loggly.password = 'your_password'
+    loggly.subdomain = 'your_subdomain'
 
-    # Ship logs
-    loggly.submit('Event data')
-    loggly.submit(json.dumps({ 'foo': 'bar' }))
-    
-    # Bulk submission
+### [Ship One Event, or a Multiline Event](https://www.loggly.com/docs/http-endpoint/)
+
+You need a customer token to ship logs (as opposed to an API token).
+
+The Content-Type header of the request will be automatically determined, so you can ship JSON by
+passing in a dict or a JSON-parseable string. Anything else gets treated as plaintext.
+
+    loggly.submit('Event data')       # Plaintext
+    loggly.submit({ 'foo': 'bar' })   # JSON
+    loggly.submit('{ "foo": "bar" }') # JSON
+
+### [Ship Bulk Data](https://www.loggly.com/docs/http-bulk-endpoint/)
+
+You need a customer token to ship logs.
+
+You can pass in a list of events...
+
+    loggly.bulk_submit([
+        'Event one',
+        'Event two',
+        'Event three'])
+
+ ...or a single string.
+
     with open('/path/to/a/big/log.file', 'r') as fh:
         loggly.bulk_submit(fh.read())
 
-    # Search logs/event retrieval
+Both forms support tagging.
 
-    # Do a search for events, get a list back when the search is done
+    loggly.bulk_submit(eventlist, tag='production')
+
+Bear in mind there are restrictions to bulk shipping. At the time of this writing, you can't send
+events more than 1MB each, and no more than 5MB per request.
+
+### [Search Events](https://www.loggly.com/docs/api-retrieving-data/)
+
+To search events, you need a subdomain and either a username/password combo or an API token.
+
+You can start a search like this, and the process will hang until search results are returned by the
+API, at which point you'll have a list of events:
+
     events = loggly.search('foo:"bar"', from='-1D', until='now')
 
-    # With paginate=True, you get an iterator instead. Each page will return after the Loggly
-    # request completes. Use this if you want to monitor progress or manually page through the data.
+Or you can set `paginate=True` to instead get a SearchIterator back immediately. Each "next" call
+will cause a request for more results to come from Loggly. This is essentially a less-automated way
+of accomplishing the same thing as above, except you can run other code between pages of results.
+Consider:
+
     search = loggly.search('foo:"bar"', from='-1D', until='now', paginate=True, pagesize=100)
     events = list()
     for page in search:
-        events.extend(events)
+        events.extend(page)
+        print('{} events collected so far!'.format(len(events)))
 
-    # Get all account info
-    loggly.account_info()
+`pagesize` is the number of results to return per page. Loggly limits this to 1,000 events at most,
+but this can sometimes cause problems as well. If you find yourself getting inexplicable errors from
+Loggly, try making your page size smaller. The effects of this setting are pretty transparent when
+`paginate` is `False`, although the page size **is** applied in that case. But when it's `True`,
+you'll get back that many events at a time from the iterator.
 
-    # Convenience access to parts of the account info
+### [Account Info](https://www.loggly.com/docs/api-account-info/)
+
+Retrieving account info requires a subdomain and either a username/password combo or an API token.
+
+    info = loggly.account_info()
+
+Calling `account_info()` also updates some convenience properties in the module:
+
     loggly.customer_tokens
+    loggly.retention
     loggly.subdomain
     loggly.volume_limit
-    loggly.retention
+
+### [Statistics](https://www.loggly.com/docs/stats-api/)
+
+The stats API requires a subdomain and either a username/password combo or an API token.
+
+Then you can call the `stats` function to get the data you need. In the fictional example below,
+you'd get back the average request time for your app.
+
+    stats = loggly.stats(stat='avg', field='json.log.request_time', query='*', frm=None, til=None)
+
+Refer to the [documentation](https://www.loggly.com/docs/stats-api/) for a complete list of valid
+statistics you can call for.
+
+## Exceptions
+
+The base class for all exceptions in this module is `loggly.LogglyError`, but it's nothing special
+and is useless except to verify that some other error is a loggly error.
+
+    print(isinstance(loggly.RequestError, loggly.LogglyError))
+
+    Output:
+      True
+
+A `RequestError` is thrown anytime this module makes a request to Loggly and gets something other
+than a `200 OK` in response. It will contain a `requests` response object as well as some
+convenience members which are extracted from it:
+
+    try:
+        stats = loggly.stats(field='json.log.bleh')
+    except loggly.RequestError as e:
+        print('{} {}\n{}'.format(e.status_code, e.reason, e.message))
+
+    Output:
+     400 BAD REQUEST
+     The requested field 'json.log.bleh' is unknown.
+
+An `InvalidStatError` is raised when you call `stats()` and provide an invalid stat value.
+
+    try:
+        stats = loggly.stats(field='json.log.rq _size', stat='avf')
+    except InvalidStatError as e:
+        print('Invalid statistic: {}'.format(e.stat))
+
+    Output:
+      Invalid statistic: avf
+
+An `AuthenticationError` is raised when you try to make an API call without providing sufficient
+credentials for the operation.
+
+    try:
+        loggly.subdomain = 'mysub'
+        loggly.user = 'myuser'
+        events = loggly.search()
+    except AuthenticationError as e:
+        print('You didn't set a password or an api_token!')
 
---
+++ loggly/__init__.py
@@ -0,0 +1,255 @@
+import json
+
+from requests import get, post
+from requests.auth import HTTPBasicAuth
+
+# Auth data
+token = None
+api_token = None
+user = None
+password = None
+
+# Account info
+tokens = None
+retention = None
+subdomain = None
+volume_limit = None
+
+# Exception related stuff
+class LogglyError(Exception):
+    pass
+
+class AuthenticationError(LogglyError):
+    pass
+
+class RequestError(LogglyError):
+    def __init__(self, response):
+        self.response = response
+        self.message = response.json()['message']
+        self.status_code = response.status_code
+        self.reason = response.reason
+
+class InvalidStatError(LogglyError):
+    def __init__(self, stat):
+        self.stat = stat
+
+# "Private" functions
+def __basicauth():
+    if user is None or password is None:
+        return None
+    return HTTPBasicAuth(user, password)
+
+def __apiauth():
+    if api_token is None:
+        return None
+    return { 'Authorization': 'Bearer {}'.format(api_token) }
+
+def __getauth():
+    auth = __apiauth()
+    if not auth:
+        auth = __basicauth()
+    return auth
+
+def __format_params(params):
+    return '&'.join([ '{}={}'.format(key, params[key]) for key in params ])
+
+
+class SearchIterator(object):
+    def __init__(self, url, auth):
+        self.url = url
+        self.auth = auth
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.url is None: raise StopIteration
+
+        response = SearchIterator.get_page(self.url, self.auth)
+        self.url = response['next'] if 'next' in response else None
+        if 'events' in response:
+            return response['events']
+        return list()
+
+    def get_page(url, auth):
+        response = None
+        # auth can be an API token header dict or a basic auth object; react accordingly
+        if isinstance(auth, HTTPBasicAuth):
+            response = get(url, auth=auth)
+        elif isinstance(auth, dict):
+            response = get(url, headers=auth)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise RequestError(response)
+        return None
+
+
+# The "exposed" functions
+def account_info():
+    global tokens, subdomain, retention, volume_limit
+
+    # Try an API token first; failing that, try HTTPBasicAuth
+    auth = __getauth()
+
+    if auth:
+        url = 'https://{}.loggly.com/apiv2/customer'.format(subdomain)
+        if isinstance(auth, HTTPBasicAuth):
+            response = get(url, auth=auth)
+        elif isinstance(auth, dict):
+            response = get(url, headers=auth)
+
+        print(response.status_code)
+        if response.status_code == 200:
+            info = response.json()
+            tokens = info['tokens']
+            subdomain = info['subdomain']
+            retention = info['subscription']['retention_days']
+            volume_limit = info['subscription']['volume_limit_mb']
+            return info
+        else:
+            raise RequestError(response)
+    else:
+        raise AuthenticationError()
+
+    return None
+
+def submit(event, tag=None):
+    if not token:
+        raise AuthenticationError()
+
+    url = 'https://logs-01.loggly.com/inputs/{}/'.format(token)
+    if tag:
+        url += 'tag/{}'.format(tag)
+
+    content_type = 'text/plain'
+    if isinstance(event, dict):
+        event = json.dumps(event)
+        content_type = 'application/x-www-form-urlencoded'
+    if isinstance(event, str):
+        try:
+            json.loads(event)
+            content_type = 'application/x-www-form-urlencoded'
+        except ValueError:
+            pass
+
+    response = post(
+        url,
+        headers={'Content-Type': content_type},
+        data=event)
+
+
+    if response.status_code == 200:
+        return True
+    else:
+        raise RequestError(response)
+
+def bulk_submit(events, tag=None):
+    if not token:
+        raise AuthenticationError()
+
+    url = 'https://logs-01.loggly.com/bulk/{}/'.format(token)
+    if tag:
+        url += 'tag/{}'.format(tag)
+
+    if isinstance(events, list):
+        events = '\n'.join(events)
+
+    print(events)
+
+    response = post(
+        url,
+        headers={'Content-Type': 'text/plain'},
+        data=events)
+
+    if response.status_code == 200:
+        return True
+    else:
+        raise RequestError(response)
+
+def search(query=None, frm=None, til=None, paginate=False, pagesize=None, order=None):
+    # Events Retrieval API: https://www.loggly.com/docs/paginating-event-retrieval-api/
+
+    # Try an API token first; failing that, try HTTPBasicAuth
+    auth = __getauth()
+
+    if auth:
+        baseurl = 'https://{}.loggly.com/apiv2/events/iterate'.format(subdomain)
+        events = list()
+        params = dict()
+        if query: params['q'] = query
+        if frm: params['from'] = frm
+        if til: params['until'] = til
+        if pagesize: params['size'] = pagesize
+        if order:
+            params['order'] = order if order in [ 'asc', 'desc' ] else None
+        if not order: params.pop('order', None)
+
+        if len(params.keys()) > 0:
+            url = '{}?{}'.format(baseurl, __format_params(params))
+        else:
+            url = baseurl
+
+        if paginate:
+            return SearchIterator(url, auth)
+        else:
+            while True:
+                response = SearchIterator.get_page(url, auth)
+                if response:
+                    if 'events' in response:
+                        events.extend(response['events'])
+                    if 'next' in response:
+                        url = response['next']
+                    else:
+                        break
+            return events
+    else:
+        raise AuthenticationError()
+
+    return None
+
+def stats(stat='all', field=None, query='*', frm=None, til=None):
+    # Validate the stat field
+    stat = stat.lower()
+    if stat not in [
+        'avg',
+        'sum',
+        'min',
+        'max',
+        'percentiles',
+        'variance',
+        'std_deviation',
+        'sum_of_squares',
+        'count',
+        'stats',
+        'extended',
+        'all' ]:
+            raise InvalidStatError(stat)
+
+    auth = __getauth()
+
+    if auth:
+        baseurl = 'https://{}.loggly.com/apiv2/stats/{}/{}'.format(subdomain, stat, field)
+        params = dict()
+        if query: params['q'] = query
+        if frm: params['from'] = frm
+        if til: params['until'] = til
+        if len(params.keys()) > 0:
+            url = '{}?{}'.format(baseurl, __format_params(params))
+        else:
+            url = baseurl
+
+        response = None
+        if isinstance(auth, dict):
+            response = get(url, headers=auth)
+        elif isinstance(auth, HTTPBasicAuth):
+            response = get(url, auth=auth)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise RequestError(response)
+    else:
+        raise AuthenticationError()
+
