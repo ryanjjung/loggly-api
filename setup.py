f5aedac7480e28af83a007beaaf3d570aa5583a9
Ryan Jung <ryanjjung@gmail.com>
2018-04-04 10:21:22 -0600

    Add dependency, setup.py, LICENCE.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='loggly-api',
+    version='0.2',
+    description="A Python library to work with Loggly's APIs",
+    url='https://github.com/ryanjjung/loggly-api',
+    author='Ryan Jung',
+    author_email='ryanjjung@gmail.com',
+    license='Apache License 2.0',
+    install_requires='requests',
+    packages=['loggly'])
+
